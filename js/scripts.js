8a39d0d4a1629bfb2ebb45683456f77c2ab963f6
Olivia Davies <odavies@vcd-ceb131-19.eastern.ewu.edu>
2017-05-18 16:13:37 -0700

    in class foolery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
-console.log('Hello, World!');
+// Template Literal
+var students = ` <section class="ui card">
+               <div class="image">
+                   <img src="http://placehold.it/400x300" alt="">
+               </div>
+               <div class="content">
+                   <h3>First Last</h3>
+                   <p class="description">
+                       <a href="">GitHub</a> and <a href="mailto:">Email</a>
+                   </p>
+               </div>
+           </section>`
+;
