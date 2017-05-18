6f108ba8a3ec4519fdf4a6854119d04861f00f08
Olivia Davies <odavies@vcd-ceb131-19.eastern.ewu.edu>
2017-05-18 16:37:12 -0700

    end of class / Version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,53 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
+// @codekit-prepend "airtable.js";
+
+// CONFIGURE & "Handshake"
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyNyK2dVB4Da7Dzo'
+});
+var base = Airtable.base('appYjgGyxE7AXQYex');
+
+//Check Check
+console.log(base);
+
+//Get Records
+base('YaHURR').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+    records.forEach(function(record) {
+        
+        //console.log('Retrieved', record.get('Name'));
+        var name = `<h1>${record.get('Name')}</h1>`;
+    
+        // Display
+        $('#students').html(students);
+    
+    
+    });
+});
+
+
+
+
+
+
 
 // Template Literal
-var students = ` <section class="ui card">
-               <div class="image">
-                   <img src="http://placehold.it/400x300" alt="">
-               </div>
-               <div class="content">
-                   <h3>First Last</h3>
-                   <p class="description">
-                       <a href="">GitHub</a> and <a href="mailto:">Email</a>
-                   </p>
-               </div>
-           </section>`
-;
+var students = `
+    <section class="ui card">
+        <div class="image">
+           <img src="http://placehold.it/400x300" alt="">
+        </div>
+       <div class="content">
+           <h3>First Last</h3>
+           <p class="description">
+               <a href="">GitHub</a> and <a href="mailto:">Email</a>
+           </p>
+       </div>
+    </section>
+`;
+
